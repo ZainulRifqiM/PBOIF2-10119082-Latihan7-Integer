368df315090e4f86ed2d612db4c81aeaa8eb4e67
Zainul Rifqi <Zainul Rifqi@LAPTOP-O1E2M14H>
2020-10-17 16:56:28 +0700

    PBOIF2-10119082-Latihan7-Integer

---
+++ src/pboif2/pkg10119082/latihan7/integer/PBOIF210119082Latihan7Integer.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119082.latihan7.integer;
+
+/**
+ *
+ * @author Zainul Rifqi
+ * NAMA     : Zainul Rifqi Muwaffaq
+ * KELAS    : IF2
+ * NIM      : 10119082
+ * Deskripsi Program : Integer
+ */
+public class PBOIF210119082Latihan7Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        System.out.println("byte : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int : " + i);
+        System.out.println("long : " + l);
+    }
+    
+}
